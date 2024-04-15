ca696493d384796f26381ee5c87913c19acfd54e
WangChowder <dpeas611@gmail.com>
2024-04-15 18:31:33 +0800

    UpData

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,10 +13,10 @@ function Footer() {
           <br />
           <div className="container d-flex justify-content-between">
               <div className="d-flex flex-column justify-content-space-evenly">
-                  <p><i className="fa-solid fa-phone"></i> {"電話: 04-229374948"}</p>
-                  <p><i className="fa-solid fa-house"></i> {"地址: 408台中市南屯區公益路二段51號18樓"}</p>
-                  <p><i className="fa-solid fa-envelope"></i> {"電子信箱: goodcare@gmail.com"}</p>
-                  <p><i className="fas fa-clock"></i> {"營業時間: 09:00-18:00"}</p>
+                  <p><i className="fa-solid fa-phone"></i> {"電話:"}&nbsp;{" 04-229374948"}</p>
+                  <p><i className="fa-solid fa-house"></i> {"地址:"}&nbsp;{" 408台中市南屯區公益路二段51號18樓"}</p>
+                  <p><i className="fa-solid fa-envelope"></i> {"電子信箱:"}&nbsp;{" goodcare@gmail.com"}</p>
+                  <p><i className="fas fa-clock"></i> {"營業時間:"}&nbsp;{" 09:00-18:00"}</p>
               </div>
               <div className="d-flex flex-column">
                   <div><img src={require("../../asset/images/index/footerlogo.png")} alt='footerLogo'/></div>
